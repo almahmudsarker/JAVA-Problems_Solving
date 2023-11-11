7aea022ff11f121bdc744f8e8008360c9faca1d3
almahmudsarker <almahmuds427@gmail.com>
2023-11-11 11:41:19 +0600

    butterfly pattern

---
+++ Advanced pattern/butterflyPattern.java
@@ -0,0 +1,41 @@
+public class butterflyPattern {
+    public static void main(String[] args) {
+        System.out.println("butterfly Pattern");
+        int n = 5;
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.err.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
